9308be8e12bfa2945e11393c9c9c1933e9643d8e
VickyPalli <pallivenkaiah897@gmail.com>
2026-02-24 19:14:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page 🚀");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.post("/data", (req, res) => {
+  res.json({
+    message: "Data received",
+    data: req.body,
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
