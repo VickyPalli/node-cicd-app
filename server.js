868c5e0893cab399beebcb393d30f7e906ca8c41
VickyPalli <pallivenkaiah897@gmail.com>
2026-03-08 15:55:36 +0530

    commiyt

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({
     message:
-      "Deploment with groovy script Jenkinsfile ...🚀",
+      "Deploment with groovy script Jenkinsfile🚀",
   });
 });
 
